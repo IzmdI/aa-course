d32247d70dfb9e3336225fe4241ce26cea3abc1e
andrei.smelov <andrei.smelov@acronis.com>
2023-08-15 16:50:24 +0300

    remove logic

--- auth/src/application/settings/auth.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    SECRET_KEY: str = "2d88f9e8985004c72ca39dea0f96e8814d7ebac05205f0abeae9c32f5d0e1c4a"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
